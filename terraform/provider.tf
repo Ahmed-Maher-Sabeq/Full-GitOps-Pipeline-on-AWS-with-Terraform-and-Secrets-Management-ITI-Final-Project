4a453e37df57394aa21806d640354c6833596594
Abdelaziz-Gbr <abdelazizkhalid@outlook.com>
2025-11-17 03:20:34 +0200

    using my repo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,8 +14,6 @@ terraform {
 # AWS Provider Configuration
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["conf"]
-  shared_credentials_files = ["creds"]
   profile                  = "default"
 
   default_tags {
